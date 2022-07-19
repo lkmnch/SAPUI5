05b483405225de8e3508cb933a8f6851e0d048a4
lkmnch <loukmane.ch@web.de>
2022-07-19 21:17:07 +0200

    Steps <19

--- webapp/component.js
+++ webapp/component.js
@@ -1,19 +1,14 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function (UIComponent, JSONModel, ResourceModel) {
+    "./controller/HelloDialog"
+], function (UIComponent, JSONModel, HelloDialog) {
     "use strict";
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-        metadata : {
-            rootView: {
-                "viewName": "sap.ui.demo.walkthrough.view.App",
-                "type": "XML",
-                "async": true,
-                "id": "app"
-            }
+        metadata: {
+            manifest: "json"
         },
-        init : function () {
+        init: function () {
             // call the init function of the parent
             UIComponent.prototype.init.apply(this, arguments);
             // set data models
@@ -25,13 +20,16 @@ sap.ui.define([
             var oModel = new JSONModel(oData);
             this.setModel(oModel);
 
-            // set i18n model
-            var i18nModel = new ResourceModel({
-                bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-                supportedLocales: [""],
-                fallbackLocale: ""
-            });
-            this.setModel(i18nModel, "i18n");
+            //set dialog
+            this._helloDialog = new HelloDialog(this.getRootControl());
+        },
+        exit: function () {
+            this._helloDialog.destroy();
+            delete this._helloDialog;
+        },
+
+        openHelloDialog : function () {
+            this._helloDialog.open();
         }
     });
 });
